6d3f7dfadc535c8af1f4fbca4f2d0741f29f0359
Keith Mellea <keithmellea@gmail.com>
2021-03-24 16:14:45 -0400

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
-
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+const { urlencoded } = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
-
-
+app.use(cookieParser());
+app.use(express.urlencoded());
+const csrfProtection = csrf({ cookie: true});
 
 const users = [
   {
@@ -16,7 +19,7 @@ const users = [
   }
 ];
 
-app.get("/", (req, res) => {
+app.get("/", csrfProtection, (req, res) => {
   res.render("index", {users});
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
