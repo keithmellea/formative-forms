cbe41201544074a6198439efa016e1683d776eb5
Alex Clough <alexcl123@gmail.com>
2021-03-24 17:33:22 -0400

    figuring out csrf

--- index.js
+++ index.js
@@ -20,13 +20,64 @@ const users = [
   }
 ];
 
-app.get("/", (req, res) => {
+app.get("/", csrfProtection, (req, res) => {
   res.render("index", {users});
 });
 
 app.get("/create", (req, res) => {
   res.render("create", {title: "create"})
 })
+
+const validateUser = (req, res, next) => {
+  const { firstName, lastName, email, password, confirmedPassword } = req.body;
+  const errors = [];
+  if (!firstName) {
+    errors.push("Please fill out the first name field");
+  }
+  if (!lastName) {
+    errors.push("Please fill out the last name field");
+  }
+  if (!email) {
+    errors.push("Please fill out the email field");
+  }
+  if (!password) {
+    errors.push("Please fill out the password field");
+  }
+  if (!confirmedPasword) {
+    errors.push("Please fill out the confirmed password field");
+  }
+
+  req.errors = errors;
+
+  next();
+};
+
+app.post("/create", csrfProtection, validateUser, (req, res) => {
+  const { firstName, lastName, email, password, confirmedPassword } = req.body;
+
+  if (req.errors.length > 0) {
+    res.render("create", {
+      title: "create",
+      errors,
+      email,
+      firstName,
+      lastName,
+      password,
+      confirmedPassword,
+    });
+    return;
+  }
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+    confirmedPassword: req.body.confirmedPassword,
+  };
+  users.push(user);
+  res.redirect("/");
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = app;
